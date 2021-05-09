ea6491f7026ffe700d6f1c57b3e9892f657986b8
Michael Hodge <michaelhdg03@gmail.com>
2021-05-09 15:23:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# InfiniteSkate
+
+An infinite scrolling skateboarding game developed with Unreal Engine 4
---
+++ Source/InfiniteSkate/InfiniteSkateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfiniteSkateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITESKATE_API AInfiniteSkateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
